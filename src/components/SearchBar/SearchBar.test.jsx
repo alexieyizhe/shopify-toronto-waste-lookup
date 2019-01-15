4e05a906c06bfa9a993c2e0e3d17289e55701256
Alex Xie <alexieyizhe@gmail.com>
2019-01-15 14:24:18 -0500

    Finish snapshot tests for rendering and incorporate jest-styled-components

--- src/components/SearchBar/SearchBar.test.jsx
+++ src/components/SearchBar/SearchBar.test.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import 'jest-styled-components';
+import { ThemeProvider } from 'styled-components';
+import { siteTheme } from '../../utils/siteData';
+
 import SearchBar from './SearchBar';
+
 
 /* eslint-disable no-undef */
 it('renders correctly', () => {
-  const tree = renderer.create(<SearchBar />).toJSON();
+  const tree = renderer.create(
+    <ThemeProvider theme={siteTheme}>
+      <SearchBar />
+    </ThemeProvider>
+  ).toJSON();
   expect(tree).toMatchSnapshot();
 });
