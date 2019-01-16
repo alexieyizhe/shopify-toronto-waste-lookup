2da93ab854a63b751820540839491101141a7813
Alex Xie <alexieyizhe@gmail.com>
2019-01-16 16:06:11 -0500

    Add hover and color for favourite star icon

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,7 +1,6 @@
 import React, { forwardRef } from 'react';
 import styled, { withTheme } from 'styled-components';
 import posed from 'react-pose';
-import Star from 'react-feather/dist/icons/star';
 
 import { ItemsContext } from '../../utils/siteContext';
 
@@ -34,6 +33,16 @@ const ComponentContainer = styled.div`
 const FavouriteButton = styled.div`
   grid-area: fav;
   width: 3%;
+
+  & svg {
+    cursor: pointer;
+    fill: ${props => props.highlight ? props.theme.colors.secondary : props.theme.colors.offBlack};
+    transition: fill 200ms ease-in-out;
+
+    &:hover {
+      fill: ${props => props.theme.colors.secondary};
+    }
+  }
 `;
 
 const TitleContainer = styled.div`
@@ -66,7 +75,12 @@ const ItemCard = forwardRef(({title, body, ith, isFavourite, theme}, innerRef) =
   <ItemsContext.Consumer>
     {({items, favs, updateFavs}) => (
       <ComponentContainer  ref={innerRef}>
-        <FavouriteButton onClick={() => updateFavs(ith, isFavourite)}><Star size="1vw" color={isFavourite ? theme.colors.secondary : "grey"}/></FavouriteButton>
+        <FavouriteButton onClick={() => updateFavs(ith, isFavourite)} highlight={isFavourite}>
+          {/* Icon source: https://www.s-ings.com/typicons/ */}
+          <svg width="1.5vw" height="1.5vw" baseProfile="tiny" version="1.2" viewBox="0 0 24 24">
+            <path d="M3.1 11.3l3.6 3.3-1 4.6c-.1.6.1 1.2.6 1.5.2.2.5.3.8.3.2 0 .4 0 .6-.1 0 0 .1 0 .1-.1l4.1-2.3 4.1 2.3s.1 0 .1.1c.5.2 1.1.2 1.5-.1.5-.3.7-.9.6-1.5l-1-4.6c.4-.3 1-.9 1.6-1.5l1.9-1.7.1-.1c.4-.4.5-1 .3-1.5s-.6-.9-1.2-1h-.1l-4.7-.5-1.9-4.3s0-.1-.1-.1c-.1-.7-.6-1-1.1-1-.5 0-1 .3-1.3.8 0 0 0 .1-.1.1L8.7 8.2 4 8.7h-.1c-.5.1-1 .5-1.2 1-.1.6 0 1.2.4 1.6z"/>
+          </svg>
+        </FavouriteButton>
         <TitleContainer>{title}</TitleContainer>
         <DescContainer dangerouslySetInnerHTML={{__html: unescapeHtml(body)}}></DescContainer> { /*  TODO: possibly implement sanitizing script that makes sure the only html elements are <ul>, <li>, and <strong> */ }
         { /*  ^^ Not really sure what the best way to sanitize this and make it safe without expending lots of effort with parsing the HTML body it returns, but the API is trusted so this should be safe */ }
